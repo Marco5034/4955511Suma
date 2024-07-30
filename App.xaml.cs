a0bb98a968b43c784cae36247d4f059c2fbefb9a
Indel <Indel@DESKTOP-BEC38OO>
2024-07-30 16:00:00 -0600

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace _4955511Suma
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
